d348c574efdc7f37ff5fbedb583221d7e5d948bf
Jakub Gaik <jakub.gaik@gmail.com>
2025-02-20 22:25:34 +0100

    add hashmap

--- utils/hash-tables.test.ts
+++ utils/hash-tables.test.ts
@@ -1,42 +1,81 @@
 import { simpleFaker } from "@faker-js/faker";
 import { describe, expect, test } from "vitest";
-import { HashSet } from "./hash-tables";
+import { HashMap, HashSet } from "./hash-tables";
 
 describe("Hash Tables", () => {
-  describe("HashSet", () => {
-    type ItemType = [string, number];
-    const item: ItemType = [
-      simpleFaker.string.alpha(),
-      simpleFaker.number.int(1),
-    ];
+  type KeyType = [string, number];
+  const key: KeyType = [simpleFaker.string.alpha(), simpleFaker.number.int(1)];
 
+  describe("HashSet", () => {
     test("allows storing non-primitive items", () => {
-      const set = new HashSet<ItemType>(
+      const set = new HashSet<KeyType>(
         (key) => key.join("_"),
         (hashed) => {
           const [str, num] = hashed.split("_");
           return [str, parseInt(num, 10)];
         },
-        [item]
+        [key]
       );
 
-      set.add([...item]);
+      set.add([...key]);
 
-      expect(set.toString()).toBe(`HashSet(1) { ${item.join("_")} }`);
+      expect(set.toString()).toBe(`HashSet(1) { ${key.join("_")} }`);
       expect(set.size).toBe(1);
-      expect(set.has(item)).toBe(true);
+      expect(set.has(key)).toBe(true);
 
-      for (const setItem of set.values()) {
-        expect(setItem).toStrictEqual(item);
+      for (const setKey of set.keys()) {
+        expect(setKey).toStrictEqual(key);
       }
 
-      set.forEach((setItem) => {
-        expect(setItem).toStrictEqual(item);
+      for (const setValues of set.values()) {
+        expect(setValues).toStrictEqual(key);
+      }
+
+      set.forEach((setKey) => {
+        expect(setKey).toStrictEqual(key);
       });
 
-      set.delete([...item]);
+      set.delete([...key]);
 
       expect(set.size).toBe(0);
+    });
+  });
+
+  describe("HashMap", () => {
+    const value = simpleFaker.string.alphanumeric();
+
+    test("allows storing and retrieving key-value pairs", () => {
+      const map = new HashMap<KeyType, string>(
+        (key) => key.join("_"),
+        (hashed) => {
+          const [str, num] = hashed.split("_");
+          return [str, parseInt(num, 10)];
+        },
+        [[key, value]]
+      );
+
+      map.set(key, value);
+
+      expect(map.toString()).toBe(
+        `HashMap(1) { ${key.join("_")} => ${value} }`
+      );
+      expect(map.size).toBe(1);
+      expect(map.has(key)).toBe(true);
+      expect(map.get(key)).toBe(value);
+
+      for (const [mapKey, mapValue] of map.entries()) {
+        expect(mapKey).toStrictEqual(key);
+        expect(mapValue).toBe(value);
+      }
+
+      map.forEach((mapValue, mapKey) => {
+        expect(mapKey).toStrictEqual(key);
+        expect(mapValue).toBe(value);
+      });
+
+      map.delete(key);
+
+      expect(map.size).toBe(0);
     });
   });
 });
--- utils/hash-tables.ts
+++ utils/hash-tables.ts
@@ -30,6 +30,10 @@ export class HashSet<T = any> {
     return Array.from(this._set.values(), this._unhashFunction).values();
   }
 
+  keys(): IterableIterator<T> {
+    return Array.from(this._set.keys(), this._unhashFunction).values();
+  }
+
   forEach(callbackfn: (value: T, set: HashSet<T>) => void): void {
     this._set.forEach((value) => {
       callbackfn(this._unhashFunction(value), this);
@@ -43,5 +47,72 @@ export class HashSet<T = any> {
   toString(): string {
     const values = Array.from(this._set.values()).join(", ");
     return `HashSet(${this.size}) { ${values} }`;
+  }
+}
+
+export class HashMap<K = any, V = any> {
+  private _map: Map<string, V>;
+
+  constructor(
+    private _hashFunction: (key: K) => string,
+    private _unhashFunction: (hashedKey: string) => K,
+    entries?: [K, V][]
+  ) {
+    this._map = new Map(
+      entries?.map(([key, value]) => [this._hashFunction(key), value])
+    );
+  }
+
+  set(key: K, value: V): this {
+    this._map.set(this._hashFunction(key), value);
+    return this;
+  }
+
+  get(key: K): V | undefined {
+    return this._map.get(this._hashFunction(key));
+  }
+
+  has(key: K): boolean {
+    return this._map.has(this._hashFunction(key));
+  }
+
+  delete(key: K): boolean {
+    return this._map.delete(this._hashFunction(key));
+  }
+
+  clear(): void {
+    this._map.clear();
+  }
+
+  keys(): IterableIterator<K> {
+    return Array.from(this._map.keys(), this._unhashFunction).values();
+  }
+
+  values(): IterableIterator<V> {
+    return this._map.values();
+  }
+
+  entries(): IterableIterator<[K, V]> {
+    return Array.from(
+      this._map.entries(),
+      ([hashedKey, value]) => [this._unhashFunction(hashedKey), value] as [K, V]
+    ).values();
+  }
+
+  forEach(callbackfn: (value: V, key: K, map: HashMap<K, V>) => void): void {
+    this._map.forEach((value, hashedKey) => {
+      callbackfn(value, this._unhashFunction(hashedKey), this);
+    });
+  }
+
+  get size(): number {
+    return this._map.size;
+  }
+
+  toString(): string {
+    const entries = Array.from(this._map.entries())
+      .map(([hashedKey, value]) => `${hashedKey} => ${value}`)
+      .join(", ");
+    return `HashMap(${this.size}) { ${entries} }`;
   }
 }
